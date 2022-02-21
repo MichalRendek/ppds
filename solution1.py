c925193640548fa1e34006e1b0b8c68c346faba2
MichalRendek <75487222+MichalRendek@users.noreply.github.com>
2022-02-22 00:32:33 +0100

    feat(sol1): commit solution one

--- solution1.py
+++ solution1.py
@@ -1,3 +1,38 @@
-from fei.ppds import Thread
+from random import randint
+from time import sleep
+from fei.ppds import Thread, Mutex
+from collections import Counter
 
 
+class Shared():
+    def __init__(self, length):
+        self.counter = 0
+        self.end = length
+        # make array longer for fix out of range bug
+        self.array = [0] * (length + 1)
+
+
+mutex = Mutex()
+
+
+def counting(shared):
+    while shared.counter < shared.end:
+        mutex.lock()
+        shared.array[shared.counter] += 1
+        # insert sleep in random time for random switching
+        # between thread during counting
+        sleep(randint(1, 10) / 1000)
+        shared.counter += 1
+        mutex.unlock()
+
+
+shared = Shared(1000)
+
+t1 = Thread(counting, shared)
+t2 = Thread(counting, shared)
+
+t1.join()
+t2.join()
+
+counter = Counter(shared.array)
+print(counter.most_common())
