12e63d931186cc7db26d2d043a8d433a5a47dddd
MichalRendek <75487222+MichalRendek@users.noreply.github.com>
2022-02-28 20:41:44 +0100

    feat: task 2 first solution

---
+++ task_2.py
@@ -0,0 +1,58 @@
+from time import sleep
+from random import randint
+from fei.ppds import Thread, Mutex, Semaphore
+from fei.ppds import print
+
+
+def rendezvous(thread_name):
+    sleep(randint(1, 10) / 10)
+    print('rendezvous: %s' % thread_name)
+
+
+def ko(thread_name):
+    print('ko: %s' % thread_name)
+    sleep(randint(1, 10) / 10)
+
+
+def barrier_example(thread_name):
+    # this two variables are set as global for global connect to their
+    global counter
+    global THREADS
+
+    while True:
+        rendezvous(thread_name)
+        m.lock()
+        counter += 1
+        if counter == THREADS:
+            s2.wait()
+            s1.signal()
+        m.unlock()
+        s1.wait()
+        s1.signal()
+
+        ko(thread_name)
+
+        m.lock()
+        counter -= 1
+        if counter == 0:
+            s1.wait()
+            s2.signal()
+        m.unlock()
+        s2.wait()
+        s2.signal()
+
+
+threads = list()
+
+m = Mutex()
+s1 = Semaphore(0)
+s2 = Semaphore(1)
+counter = 0
+THREADS = 5
+
+for i in range(THREADS):
+    t = Thread(barrier_example, 'Thread %d' % i)
+    threads.append(t)
+
+for t in threads:
+    t.join()
